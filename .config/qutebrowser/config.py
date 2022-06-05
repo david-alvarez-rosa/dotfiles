16da3a7bcf8c02b4b477908da19597bc8d41ccc9
David Álvarez Rosa <david@alvarezrosa.com>
2022-06-05 10:24:45 +0200

    [Qutebrowser] Multiple updates

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -18,8 +18,8 @@ config.bind('<Ctrl-y>', 'insert-text {primary}')
 config.bind('<Ctrl-v>', 'scroll-page 0 0.5')
 config.bind('<Alt-v>', 'scroll-page 0 -0.5')
 
-config.bind('<Alt-Shift-,>', 'scroll-to-perc 0')
-config.bind('<Alt-Shift-.>', 'scroll-to-perc')
+config.bind('<Alt-,>', 'scroll-to-perc 0')
+config.bind('<Alt-.>', 'scroll-to-perc')
 
 config.bind('<Alt-x>', 'set-cmd-text :')
 config.bind('<Ctrl-x><Ctrl-c>', 'quit')
@@ -31,7 +31,7 @@ config.bind('<Ctrl-h>', 'set-cmd-text -s :help')
 
 config.bind('<Ctrl-Space>', 'mode-enter caret')
 
-config.bind('<Ctrl-x><Ctrl-e>', 'open-editor')
+config.bind('<Ctrl-x><Ctrl-e>', 'edit-text')
 
 config.bind('<Ctrl-x><Ctrl-v>', 'reload')
 
@@ -48,8 +48,6 @@ config.bind('<Ctrl-Alt-j>', 'hint all tab')
 
 config.bind('<Ctrl-s>', 'set-cmd-text /')
 config.bind('<Ctrl-r>', 'set-cmd-text ?')
-config.bind('<Alt-n>', 'search-next')
-config.bind('<Alt-p>', 'search-prev')
 
 config.bind('<Ctrl-]>', 'tab-next')
 config.bind('<Ctrl-[>', 'tab-prev')
@@ -79,16 +77,25 @@ c.input.insert_mode.plugins = False
 
 c.input.forward_unbound_keys = 'all'
 
-config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
-config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
+config.bind('<Ctrl-f>', 'fake-key <Right>', mode='command')
+config.bind('<Ctrl-b>', 'fake-key <Left>', mode='command')
+config.bind('<Ctrl-a>', 'fake-key <Home>', mode='command')
+config.bind('<Ctrl-e>', 'fake-key <End>', mode='command')
+config.bind('<Ctrl-n>', 'fake-key <Down>', mode='command')
+config.bind('<Ctrl-p>', 'fake-key <Up>', mode='command')
+config.bind('<Alt-f>', 'fake-key <Ctrl-Right>', mode='command')
+config.bind('<Alt-b>', 'fake-key <Ctrl-Left>', mode='command')
+config.bind('<Ctrl-d>', 'fake-key <Delete>', mode='command')
+config.bind('<Alt-d>', 'fake-key <Ctrl-Delete>', mode='command')
+config.bind('<Alt-backspace>', 'fake-key <Ctrl-Backspace>', mode='command')
+config.bind('<Ctrl-w>', 'fake-key <Ctrl-backspace>', mode='command')
+config.bind('<Ctrl-y>', 'insert-text {primary}', mode='command')
 
 config.bind('<Alt-p>', 'command-history-prev', mode='command')
 config.bind('<Alt-n>', 'command-history-next', mode='command')
 
 config.bind('<Ctrl-s>', 'search-next', mode='command')
 config.bind('<Ctrl-r>', 'search-prev', mode='command')
-config.bind('<Alt-n>', 'search-next', mode='command')
-config.bind('<Alt-p>', 'search-prev', mode='command')
 
 config.bind('<Ctrl-g>', 'mode-leave', mode='command')
 
@@ -101,6 +108,17 @@ config.bind('<Ctrl-g>', 'mode-leave', mode='hint')
 config.bind('<Alt-w>', 'yank selection', mode='caret')
 config.bind('<Ctrl-Space>', 'selection-toggle', mode='caret')
 config.bind('<Ctrl-g>', 'mode-leave', mode='caret')
+
+config.bind('<Ctrl-f>', 'move-to-next-char', mode='caret')
+config.bind('<Ctrl-b>', 'move-to-prev-char', mode='caret')
+config.bind('<Ctrl-p>', 'move-to-prev-line', mode='caret')
+config.bind('<Ctrl-n>', 'move-to-next-line', mode='caret')
+config.bind('<Alt-f>', 'move-to-next-word', mode='caret')
+config.bind('<Alt-b>', 'move-to-prev-word', mode='caret')
+config.bind('<Ctrl-e>', 'move-to-end-of-line', mode='caret')
+config.bind('<Ctrl-a>', 'move-to-start-of-line', mode='caret')
+config.bind('<Alt-.>', 'move-to-end-of-document', mode='caret')
+config.bind('<Alt-,>', 'move-to-start-of-document', mode='caret')
 
 c.editor.command = ['emacsclient', '{}']
 
