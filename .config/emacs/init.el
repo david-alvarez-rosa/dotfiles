d54aacb666b21c7d2c2278ab04211ca2dc8304f0
David Álvarez Rosa <david@alvarezrosa.com>
2022-11-14 19:42:52 +0100

    Revert "[Emacs] Use the Garbage Collector Magic Hack package for smart GC"
    This reverts commit f8be41c185fcbb61b9d4d14067a02e87f33f8c95.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,8 @@
-(use-package gcmh
-  :demand t
-  :init
-  (gcmh-mode 1))
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (expt 2 23))))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
